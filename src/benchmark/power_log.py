3bd4c4de6b62ca7776222ca0e3565eb7cc49335f
JoshR-Eng <joshrawlinson05@outlook.com>
2026-02-28 17:13:48 +0000

    init power logger

---
+++ src/benchmark/power_log.py
@@ -0,0 +1,57 @@
+"""
+Power profiling log using NVIDIA tegrastats binary.
+Operates via subprocess and parses the output to 
+extract power consumption data.
+"""
+
+# =================================================
+#                   Imports
+# =================================================
+import subprocess
+import time
+from libpath import Path
+
+
+# =================================================
+#                  PowerLog Class
+# =================================================
+
+class TegrastatsLogger:
+    def __init__(self, log_path: Path, interval_ms: int=50):
+        """
+        Args:
+            log_path (Path): Desired path of log file
+            interval_ms (int): Sampling interval in milliseconds
+        """
+        self.log_file = log_file
+        self.interval_ms = interval_ms
+        self.process = None
+
+
+    def __enter__(self):
+        """
+        Start the tegrastats subprocess in background
+        """
+
+        # Ensure Output Dir exists
+        self.log_file.parent.mkdir(parents=True, exist_ok=True)
+
+
+        # Start tegrastats subprocess
+        cmd = [
+                "tegrastats",
+                "--interval", str(self.interval_ms),
+                "--logfile", str(self.log_file)
+                ]
+        self.process = subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        return self
+
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Kill the tegrastats process once inference finsishes
+        """
+        if self.process:
+            self.process.terminate()
+            self.process.wait()
